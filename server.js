111aa21fe79a5ddde56947875e1c085b523d783a
tsorak <karost0249@gmail.com>
2022-06-22 19:11:05 +0200

    add server, sockets, functionality

---
+++ server.js
@@ -0,0 +1,121 @@
+import { serve } from "https://deno.land/std/http/mod.ts";
+
+const lines = [], activeSockets = [];
+
+const pushLine = (line) => {
+    lines.push(line);
+    pushNewLineToAllSockets(JSON.stringify(line));
+}
+const getLines = () => {
+    return lines;
+}
+const pushNewLineToAllSockets = (line) => {
+    activeSockets.forEach(socket => {
+        if (socket.readyState !== 1) return;
+        socket.send(line);
+    })
+}
+
+function logError(msg) {
+    console.log(msg);
+    Deno.exit(1);
+}
+function handleConnected(socket) {
+    console.log("Connected to client ...");
+    activeSockets.push(socket);
+    socket.send(JSON.stringify(getLines()));
+}
+function handleMessage(ws, data) {
+    const parsedData = JSON.parse(data);
+    if (typeof parsedData === "object") {
+        pushLine(parsedData);
+    }
+    console.log("CLIENT >>", parsedData, lines.length);
+    // const reply = "No reply";
+    // if (reply === "exit") {
+    //     return ws.close();
+    // }
+    // ws.send(reply);
+}
+function handleError(e) {
+    console.log(e instanceof ErrorEvent ? e.message : e.type);
+}
+async function reqHandler(req) {
+    if (req.headers.get("upgrade") != "websocket") {
+        return new Response(null, { status: 501 });
+    }
+    const { socket: ws, response } = Deno.upgradeWebSocket(req);
+    ws.onopen = () => handleConnected(ws);
+    ws.onmessage = (m) => handleMessage(ws, m.data);
+    ws.onclose = () => console.log("Disconnected from client ...");
+    ws.onerror = (e) => handleError(e);
+    return response;
+}
+console.log("Waiting for client ...");
+serve(reqHandler, { port: 8080 });
+
+// import { WebSocketServer } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
+
+// const lines = [];
+// const wss = new WebSocketServer(8080);
+// wss.on("connection", (ws) => {
+//     // console.log("New connection:", ws._events);
+//     // redraw();
+//     ws.send("Pong!");
+//     setTimeout(() => {
+//         ws.send("LeTroll");
+//     }, 1000);
+
+//     setTimeout(() => {
+//         ws.send("Hohoho");
+//     }, 2000);
+
+//     ws.on("message", (message) => {
+//         ws.send("reee");
+//         console.log("Recieved:", message);
+//         lines = JSON.parse(message);
+//         console.log(lines);
+//     });
+// });
+
+// function redraw() {
+//     console.clear()
+//     console.log(wss.clients);
+// }
+
+// import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+// const router = new Router();
+
+// router
+//     .post("/", async (ctx) => {
+//         const payload = await ctx.request.body().value;
+//         console.log(payload);
+
+//         ctx.response.type = "json";
+//         ctx.response.body = { message: "Recieved data" }
+//     })
+
+// const app = new Application();
+
+// app.use(async (ctx, next) => {
+//     //LOG
+//     console.log("NEW", ctx.request.method, "FROM", ctx.request.ip, "TO", ctx.request.url.pathname);
+//     // console.log(ctx.request);
+
+//     //CORS
+//     ctx.response.headers.set("Access-Control-Allow-Origin", ctx.request.headers.get("origin"));
+//     ctx.response.headers.set("Access-Control-Allow-Credentials", true);
+//     ctx.response.headers.set("Access-Control-Allow-Headers", ['content-type', '*']);
+
+//     if (ctx.request.method === "OPTIONS") {
+//         ctx.response.status = 200;
+//         return;
+//     }
+
+//     await next();
+// });
+
+// app.use(router.routes());
+
+// await app.listen({ port: 8080 });
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,33 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
+		let socket = new WebSocket('ws://192.168.1.2:8080');
+
+		socket.addEventListener('message', (e) => {
+			const parsedData = JSON.parse(e.data);
+			// console.dir(parsedData);
+			if (Array.isArray(parsedData)) {
+				initialRedraw(parsedData);
+			} else if (typeof parsedData === 'object') {
+				console.dir(linesArray[linesArray.length - 1]);
+				drawIncomingLine(parsedData);
+			} else {
+				console.error('wtf is this? -->', parsedData);
+			}
+		});
+
+		socket.addEventListener('open', (e) => {
+			console.log(e);
+		});
+
+		socket.addEventListener('close', (e) => {
+			console.log('CLOSED:', e);
+		});
+
+		function sendLineOverSocket(line) {
+			socket.send(JSON.stringify(line));
+		}
+
 		// SETTING ALL VARIABLES
 
 		let isMouseDown = false;
@@ -24,17 +51,17 @@
 
 		document.getElementById('canvasUpdate').addEventListener('click', function () {
 			createCanvas();
-			redraw();
+			// redraw();
 		});
 		document.getElementById('colorpicker').addEventListener('change', function () {
-			console.log("xd");
+			console.log('xd');
 			currentColor = this.value;
 		});
 		document.getElementById('bgcolorpicker').addEventListener('change', function () {
-			ctx.fillStyle = this.value;
-			ctx.fillRect(0, 0, canvas.width, canvas.height);
-			redraw();
-			currentBg = ctx.fillStyle;
+			// ctx.fillStyle = this.value;
+			// ctx.fillRect(0, 0, canvas.width, canvas.height);
+			// redraw();
+			// currentBg = ctx.fillStyle;
 		});
 		document.getElementById('controlSize').addEventListener('change', function () {
 			currentSize = this.value;
@@ -42,8 +69,8 @@
 		});
 		document.getElementById('saveToImage').addEventListener(
 			'click',
-			function () {
-				downloadCanvas(this, 'canvas', 'masterpiece.png');
+			(e) => {
+				downloadCanvas(e.target, 'canvas', 'masterpiece.png');
 			},
 			false
 		);
@@ -69,6 +96,61 @@
 				ctx.lineTo(linesArray[i].x, linesArray[i].y);
 				ctx.stroke();
 			}
+		}
+
+		function initialRedraw(data) {
+			for (let i = 1; i < data.length; i++) {
+				ctx.beginPath();
+				ctx.moveTo(data[i - 1].x, data[i - 1].y);
+				ctx.lineWidth = data[i].size;
+				ctx.lineCap = 'round';
+				ctx.strokeStyle = data[i].color;
+				ctx.lineTo(data[i].x, data[i].y);
+				ctx.stroke();
+			}
+			linesArray = data;
+		}
+
+		function drawIncomingLine(line) {
+			const lastLine = linesArray[linesArray.length - 1] ? linesArray[linesArray.length - 1] : {};
+
+			if (line !== {}) {
+				if (Object.keys(lastLine).length === 0) {
+					//WE'RE STARTING A NEW LINE (INCOMING SOCKET HAS CLICKED AND IS HOLDING MOUSE0 DOWN)
+					ctx.beginPath();
+					ctx.moveTo(line.x, line.y);
+					ctx.lineWidth = line.size;
+					ctx.lineCap = 'round';
+					ctx.strokeStyle = line.color;
+				} else if (Object.keys(lastLine).length === 4) {
+					ctx.lineTo(line.x, line.y);
+					ctx.stroke();
+				}
+			}
+
+			linesArray.push(line);
+			// if (Object.keys(line).length === 4) {
+			// 	ctx.lineTo(line.x, line.y);
+			// 	ctx.stroke();
+			// }
+			// if (line === {}) {
+			// 	//SOCKET HAS LET GO OF MOUSE0
+			// }
+			// if (Object.keys(line).length == 0) {
+			// 	if (linesArray.length > 0) {
+
+			// 	}
+			// } else {
+			// 	let currentPosition = getMousePos(canvas, evt);
+			// 	// ctx.moveTo(currentPosition.x, currentPosition.y);
+			// 	// ctx.beginPath();
+			// 	// ctx.lineWidth = currentSize;
+			// 	// ctx.lineCap = 'round';
+			// 	// ctx.strokeStyle = currentColor;
+
+			// 	// ctx.lineTo(currentPosition.x, currentPosition.y);
+			// 	// ctx.stroke();
+			// }
 		}
 
 		// DRAWING EVENT HANDLERS
@@ -106,7 +188,7 @@
 
 		function save() {
 			localStorage.removeItem('savedCanvas');
-			localStorage.setItem('savedCanvas', JSON.stringify(linesArray));
+			// localStorage.setItem('savedCanvas', JSON.stringify(linesArray));
 			console.log('Saved canvas!');
 		}
 
@@ -151,14 +233,14 @@
 		// ON MOUSE DOWN
 
 		function mousedown(canvas, evt) {
-			let mousePos = getMousePos(canvas, evt);
+			// let mousePos = getMousePos(canvas, evt);
 			isMouseDown = true;
-			let currentPosition = getMousePos(canvas, evt);
-			ctx.moveTo(currentPosition.x, currentPosition.y);
-			ctx.beginPath();
-			ctx.lineWidth = currentSize;
-			ctx.lineCap = 'round';
-			ctx.strokeStyle = currentColor;
+			// let currentPosition = getMousePos(canvas, evt);
+			// ctx.moveTo(currentPosition.x, currentPosition.y);
+			// ctx.beginPath();
+			// ctx.lineWidth = currentSize;
+			// ctx.lineCap = 'round';
+			// ctx.strokeStyle = currentColor;
 		}
 
 		// ON MOUSE MOVE
@@ -167,15 +249,20 @@
 			if (isMouseDown && allowedToDraw) {
 				allowedToDraw = false;
 				let currentPosition = getMousePos(canvas, evt);
-				ctx.lineTo(currentPosition.x, currentPosition.y);
-				ctx.stroke();
-				store(currentPosition.x, currentPosition.y, currentSize, currentColor);
+				// ctx.lineTo(currentPosition.x, currentPosition.y);
+				// ctx.stroke();
+				// store(currentPosition.x, currentPosition.y, currentSize, currentColor);
 
-				console.log(linesArray);
+				// console.log(linesArray);
+
+				sendLineOverSocket(
+					compileLine(currentPosition.x, currentPosition.y, currentSize, currentColor)
+				);
+
 				setTimeout(() => {
 					allowedToDraw = true;
 					// console.log("XD");
-				}, 1000/updateRate);
+				}, 1000 / updateRate);
 			}
 		}
 
@@ -191,12 +278,33 @@
 			linesArray.push(line);
 		}
 
+		//COMPILE LINE DATA
+
+		function compileLine(x, y, s, c) {
+			const line = {
+				x: x,
+				y: y,
+				size: s,
+				color: c
+			};
+			return line;
+		}
+
 		// ON MOUSE UP
 
 		function mouseup() {
 			isMouseDown = false;
 			store();
+			sendLineOverSocket({});
 		}
+
+		document.getElementById('save')?.addEventListener('click', () => save());
+		document.getElementById('load')?.addEventListener('click', () => load());
+		document.getElementById('clearCache')?.addEventListener('click', function () {
+			localStorage.removeItem('savedCanvas');
+			linesArray = [];
+			console.log('Cache cleared!');
+		});
 	});
 </script>
 
@@ -210,22 +318,18 @@
 			<h3>Bg Color</h3>
 			<input type="color" value="#ffffff" id="bgcolorpicker" class="" />
 		</div>
-	
+
 		<div class="">
 			<h3>Tools</h3>
-			<button id="eraser" class=""
-				>➖<span class="" aria-hidden="true" /></button
-			>
-			<button id="clear" class=""
-				>🚮<span class="" aria-hidden="true" /></button
-			>
+			<button id="eraser" class="">➖<span class="" aria-hidden="true" /></button>
+			<button id="clear" class="">🚮<span class="" aria-hidden="true" /></button>
 		</div>
-	
+
 		<div class="">
 			<h3>Size (<span id="showSize">5</span>)</h3>
 			<input type="range" min="1" max="50" value="5" step="1" id="controlSize" />
 		</div>
-	
+
 		<div class="">
 			<h3>Canvas</h3>
 			<div class="flex">
@@ -240,17 +344,17 @@
 				<input type="button" class="" value="Update" id="canvasUpdate" />
 			</div>
 		</div>
-		<!-- <div class="Storage">
+		<div class="">
 			<h3>Storage</h3>
-			<input type="button" value="Save" class="" id="save" />
-			<input type="button" value="Load" class="" id="load" />
-			<input type="button" value="Clear" class="" id="clearCache" />
-		</div> -->
+			<input type="button" value="Save" class="cursor-pointer" id="save" />
+			<input type="button" value="Load" class="cursor-pointer" id="load" />
+			<input type="button" value="Clear" class="cursor-pointer" id="clearCache" />
+		</div>
 		<div class="">
 			<!-- <h3>Extra</h3> -->
 			<a id="saveToImage" class="">Download</a>
 		</div>
 	</div>
-	<canvas id="canvas" class="m-2"></canvas>
+	<canvas id="canvas" class="m-2" />
 	<p id="linesDebug">xd</p>
 </div>
