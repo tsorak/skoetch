bf45ee735d8b80367059722b38668575e525917c
tsorak <karost0249@gmail.com>
2022-08-20 11:04:42 +0200

    Broadcast messages to clients in the same room

--- routes/[roomCode].tsx
+++ routes/[roomCode].tsx
@@ -1,9 +1,10 @@
 /** @jsx h */
 import { h } from "preact";
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { IS_BROWSER } from "$fresh/runtime.ts";
+// import { IS_BROWSER } from "$fresh/runtime.ts";
 import Socket from "../islands/Socket.tsx";
-import { roomExists } from "@/communication/roomHandler.ts"
+import { roomExists, joinRoom, updateRoomData } from "@/communication/roomHandler.ts"
+import { storeSocket, removeSocket } from "@/communication/socketHandler.ts"
 
 // interface Data {
 //   results: string[];
@@ -24,6 +25,7 @@ export const handler: Handlers = {
       //Request isn't trying to upgrade to websocket.
       return ctx.render();
     }
+    socket.roomID = roomID;
     return socketHandler({response, socket});
   },
 };
@@ -31,22 +33,79 @@ export const handler: Handlers = {
 function socketHandler(data: {response: Response, socket: WebSocket}): Response {
   const {response, socket} = data;
 
-  socket.onopen = () => console.log("socket opened");
-  socket.onmessage = (e) => {
-    let parsed;
-    try {
-      parsed = JSON.parse(e.data);
-    } catch (error) {
-      parsed = e.data;
-    }
+  socket.onopen = () => handleConnected(socket);
+  socket.onmessage = (m) => handleMessage(socket, m.data);
+  // {
+  //   let parsed;
+  //   try {
+  //     parsed = JSON.parse(e.data);
+  //   } catch (error) {
+  //     parsed = e.data;
+  //   }
 
-    console.log("socket message:", parsed);
-    socket.send(parsed);
-  };
+  //   console.log("socket message:", parsed);
+  //   socket.send(parsed);
+  // };
   socket.onerror = (e) => console.log("socket errored:", e);
-  socket.onclose = () => console.log("socket closed");
+  socket.onclose = () => handleDisconnect(socket);
   
   return response;
+}
+
+function handleConnected(socket) {
+  console.log("Connected to client ...");
+  socket.id = crypto.randomUUID();
+  // socket.currentRoom = (socket.currentRoom !== undefined) ? socket.currentRoom : "default";
+  console.log(socket.roomID, socket.id);
+  storeSocket(socket.id, socket);
+  // socket.send(JSON.stringify(getLines()));
+  console.log("joining room");
+  joinRoom(socket.roomID, socket.id);
+}
+function handleDisconnect(socket) {
+  console.log("Disconnected from client ...");
+  removeSocket(socket.id, socket.roomID);
+}
+
+function handleMessage(socket, data) {
+  let parsedData;
+  try {
+    parsedData = JSON.parse(data);
+  } catch (error) {
+    parsedData = data;
+  }
+  console.log("Socket id (socket.id):", socket.id, "Socket room (socket.roomID):", socket.roomID);
+  if (typeof parsedData === "string") {
+    updateRoomData(socket.roomID, parsedData);
+  }
+  // if (typeof parsedData === "object") {
+  //     if (!parsedData.type) {
+  //         //Default message is treated as a line object
+  //         room.updateRoomData(ws.currentRoom, parsedData) //+ id when roomdata = (roomID, [clients:[lines]])
+  //     }
+  //     if (parsedData.type) {
+  //         switch (parsedData.type) {
+  //             case MESSAGE_TYPE.NEWROOM: //NEW ROOM
+  //                 console.log("switch newroom");
+  //                 room.newRoom(ws.currentRoom, ws.id);
+  //                 break;
+
+  //             case MESSAGE_TYPE.JOINROOM: //JOIN ROOM
+  //                 console.log("switch joinroom");
+  //                 room.joinRoom(ws.currentRoom, ws.id);
+  //                 break;
+
+  //             case MESSAGE_TYPE.LEAVEROOM: //LEAVE ROOM
+  //                 console.log("switch leaveroom");
+  //                 room.leaveRoom(ws.currentRoom, ws.id);
+  //                 break;
+
+  //             default:
+  //                 break;
+  //         }
+  //     }
+  // }
+  console.log("CLIENT >>", parsedData);
 }
 
 export default function RoomCanvasPage(props: PageProps) {
